e47cbeff7b6ce29678df47d9fcdf6e2028ac8c6d
Oleksandr Trukhin <trukhino@ou.org>
2024-04-16 21:33:02 +0200

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+	<header>
+		<h1>My App</h1>
+	</header>
+</template>
+<style scoped>
+header {
+	position: fixed;
+	width: 100%;
+}
+</style>
---
+++ src/pages/cart.vue
@@ -0,0 +1,6 @@
+<template>
+	<Header></Header>
+</template>
+<script setup lang="ts">
+import Header from "../components/Header.vue";
+</script>
