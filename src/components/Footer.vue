637bcddd10a5efbb21e3e6ecdb63423ae8480cac
Oleksandr Trukhin <trukhino@ou.org>
2024-04-16 22:06:37 +0200

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,3 +3,13 @@
 		<p>&copy; 2024 Магазин квітів</p>
 	</footer>
 </template>
+<style scoped>
+footer {
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	bottom: 0;
+	padding: 0 16px;
+}
+</style>
