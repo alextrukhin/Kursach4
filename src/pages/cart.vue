05b6379185dcb01d19cb2429a38fa05ef9fc370e
Jasokaa <anna.kuts.yu@gmail.com>
2024-04-28 22:03:27 +0300

    Update cart.vue

--- src/pages/cart.vue
+++ src/pages/cart.vue
@@ -1,17 +1,22 @@
 <template>
-	<h1>Корзина</h1>
-	<div>
-		<div v-for="elem in cart" :key="elem.productID">
-			<RouterLink :to="`/flower/${elem.productID}`">
-				<img :src="elem.product.image" />
-			</RouterLink>
-			<div>
-				<h2>{{ elem.product.name }}</h2>
-				<p>{{ elem.product.price }}</p>
-				<p>{{ elem.quantity }}</p>
-			</div>
-		</div>
-	</div>
+  <h1>Корзина</h1>
+  <div>
+    <div v-for="elem in cart" :key="elem.productID">
+      <RouterLink :to="`/flower/${elem.productID}`">
+        <img v-if="elem.product.image" :src="elem.product.image" />
+        <img
+          v-else
+          src="https://via.placeholder.com/350x400?text=No+image"
+          alt="Default"
+        />
+      </RouterLink>
+      <div>
+        <h2>{{ elem.product.name }}</h2>
+        <p>{{ elem.product.price }}</p>
+        <p>{{ elem.quantity }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
 import { useProductsStore } from "../stores/products";
@@ -19,13 +24,13 @@ import { computed } from "vue";
 const productsStore = useProductsStore();
 
 const cart = computed(() =>
-	productsStore.carted.content.map((cartElem) => {
-		return {
-			...cartElem,
-			product: productsStore.products.find(
-				(product) => product.id === cartElem.productID
-			)!,
-		};
-	})
+  productsStore.carted.content.map((cartElem) => {
+    return {
+      ...cartElem,
+      product: productsStore.products.find(
+        (product) => product.id === cartElem.productID
+      )!,
+    };
+  })
 );
 </script>
