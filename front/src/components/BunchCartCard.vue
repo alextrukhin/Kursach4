73818a92b6b7e4901420892caa37d3ac2ea43329
Jasokaa <anna.kuts.yu@gmail.com>
2024-05-17 20:17:27 +0300

    Update BunchCartCard.vue

---
+++ front/src/components/BunchCartCard.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+defineProps({
+  photoUrl: String,
+  name: String,
+  price: Number,
+});
+</script>
+<template>
+  <div class="product-card">
+    <div class="image-container">
+      <img v-if="photoUrl" :src="photoUrl" :alt="name" class="product-image" />
+      <img
+        v-else
+        src="https://via.placeholder.com/350x400?text=No+image"
+        alt="Default"
+        class="product-image"
+      />
+    </div>
+    <div class="text-container">
+      <div class="product-name">{{ name }}</div>
+      <div class="product-price" v-if="price">
+        {{ "total price - $" + price }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.product-card {
+  width: 350px;
+  height: 435px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 10px;
+  background: #f9eff2;
+}
+.product-card:hover {
+  box-shadow: 1px 2px 3px 0px rgba(55, 0, 23, 0.1);
+}
+.image-container {
+  width: 330px;
+  height: 330px;
+  border-radius: 7px;
+  background: #fff;
+  margin-top: 20px;
+  margin-bottom: 10px;
+}
+.product-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 7px;
+}
+.text-container {
+  width: 330px;
+  height: 60px;
+}
+.product-name {
+  color: #370017;
+  text-align: center;
+  text-overflow: ellipsis;
+  font-family: Montserrat;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  text-transform: uppercase;
+  margin-bottom: 5px;
+}
+.product-price {
+  color: #370017;
+  text-align: center;
+  font-family: Montserrat;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 300;
+  line-height: normal;
+  text-transform: uppercase;
+  margin: 0 auto 5px auto;
+}
+</style>
